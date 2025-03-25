42db901cf7da9fdf068c6a40e9bef312691b1b55
Harshitha Sappidi <sappidi.h@northeastern.edu>
2025-03-25 00:26:20 -0400

    Adding cloud watch changes

--- src/services/metrics.js
+++ src/services/metrics.js
@@ -1,27 +1,6 @@
-// const StatsD = require("node-statsd");
-// const client = new StatsD({ host: "127.0.0.1", port: 8125 });
-
-// // Track API Calls
-// function trackApiUsage(apiName, durationMs) {
-//   client.increment(`webapp.api.${apiName}.count`); // Count API calls
-//   client.timing(`webapp.api.${apiName}.response_time`, durationMs); // API response time
-// }
-
-// // Track Database Query Performance
-// function trackDatabaseQuery(queryName, durationMs) {
-//   client.timing(`webapp.db.${queryName}.execution_time`, durationMs);
-// }
-
-// // Track S3 Service Calls
-// function trackS3Call(operation, durationMs) {
-//   client.timing(`webapp.s3.${operation}.execution_time`, durationMs);
-// }
-
-// module.exports = { trackApiUsage, trackDatabaseQuery, trackS3Call };
-
 // metrics.js
 
-const client = require('./statdClient');
+const client = require('./statsdClient');
 
 const trackApiUsage = (apiPath, duration) => {
   // Increment API call count
