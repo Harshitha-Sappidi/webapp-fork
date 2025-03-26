6a85600cdf82dc44c293214ba4ba8fbf388c7720
Harshitha Sappidi <sappidi.h@northeastern.edu>
2025-03-26 02:20:26 -0400

    cloudwatch config changes

--- packer/webapp_setup.sh
+++ packer/webapp_setup.sh
@@ -37,22 +37,14 @@ sudo mv /tmp/webapp.service /etc/systemd/system
 sudo systemctl daemon-reload
 
 
-# # Install and Start CloudWatch Agent
-# echo "Checking for CloudWatch Agent..."
-if [[ ! -f /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl ]]; then
-    echo "CloudWatch Agent not found. Installing..."
-    cd /tmp
-    wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-    sudo dpkg -i amazon-cloudwatch-agent.deb
-    sudo systemctl enable amazon-cloudwatch-agent
-    sudo systemctl start amazon-cloudwatch-agent
-    sudo systemctl status amazon-cloudwatch-agent
-
-fi
-
-# echo "Starting CloudWatch Agent with custom config..."
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config -m ec2 -c file:/opt/csye6225/webapp/packer/cloudwatch-config.json -s
+# Install and Start CloudWatch Agent
+echo "CloudWatch Agent Installing..."
+cd /tmp || exit
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo -E dpkg -i amazon-cloudwatch-agent.deb
+sudo systemctl enable amazon-cloudwatch-agent
+sudo systemctl start amazon-cloudwatch-agent
+sudo systemctl status amazon-cloudwatch-agent
 
 # Restart, enable, and check the status of the webapp service
 sudo systemctl restart webapp.service
